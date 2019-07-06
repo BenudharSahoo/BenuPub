6075ebec2ec5e96534f0a5597e9c8464fef1fc8d
Benudhar Sahoo <benudharsahoo@gmail.com>
2019-07-06 12:27:13 -0700

    Add files via upload

---
+++ CreateSelfSigningCertForCodeSigning.ps1
@@ -0,0 +1,13 @@
+﻿# Requires -Version 5
+
+# this is where the cert file will be saved
+$Path = "C:\temp\codeSignCert.pfx"
+
+# you'll need this password to load the PFX file later
+$Password = Read-Host -Prompt 'Enter new password to protect certificate' -AsSecureString
+
+# create cert, export to file, then delete again
+$cert = New-SelfSignedCertificate -KeyUsage DigitalSignature -KeySpec Signature -FriendlyName 'Benudhar Sahoo' -Subject CN='Benudhar Sahoo Code Signing Certificate' -KeyExportPolicy ExportableEncrypted -CertStoreLocation Cert:\CurrentUser\My -NotAfter (Get-Date).AddYears(10) -TextExtension @('2.5.29.37={text}1.3.6.1.5.5.7.3.3')
+$cert | Export-PfxCertificate -Password $Password -FilePath $Path
+
+# After the certificate is created, install it to your personal store. 
---
+++ HowToSignPowershellCodeUsingCert.ps1
@@ -0,0 +1,12 @@
+﻿# How to sing a code using a code signing certificate?
+# Get the cert to a vairable
+$MyCodeSigncert =(Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)[0]
+
+# Sign the certificate using the code signig certificate
+Set-AuthenticodeSignature "C:\temp\Benu.ps1" -Certificate $MyCodeSigncert
+
+#Verify the signature
+Get-AuthenticodeSignature "C:\temp\Benu.ps1" | Format-Table -AutoSize
+
+#If Somebody has ediated the code and you do a verify signature using above command you will get status HashMismatch
+
